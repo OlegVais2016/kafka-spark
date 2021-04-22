844f51a408549d6d9599ec60b86e8cd02d5c2886
Oleg Vaiser <aly9681082@gmail.com>
2021-04-22 17:33:56 +0300

    first commit

---
+++ kafka-prod/src/main/java/com/weisser/kafkaprod/KafkaProdApplication.java
@@ -0,0 +1,13 @@
+package com.weisser.kafkaprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProdApplication.class, args);
+	}
+
+}
